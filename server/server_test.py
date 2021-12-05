3aacde22dbc3214bb86bfce70a2f85d548320cf2
dvir <dvir.pollak@gmail.com>
2021-12-05 19:58:45 +0200

    up to date 19:58 all is working

--- server/server_test.py
+++ server/server_test.py
@@ -7,37 +7,40 @@ import time
 import string
 import random
 
-REC_ACK = 3
+TOKEN_IN_BYTES = b"$"
+TOKEN_STR = "$"
+REC_ACK = sys.getsizeof(b"ACK")
 PORT = int(sys.argv[1])
-
+linox_token = "/"
 FORMAT = "utf-8"
 SERVER_DATA_PATH = os.getcwd()
 
-
+#--------------------------------
+#
 def return_path(root, identifier):
     root_list = root.split("/")
     i = root_list.index(identifier)
     path = ''
-    for x in root_list[i+1:]:
+    for x in root_list[i + 1:]:
         path += "/"
         path += x
     return path
+
 
 # ---------------------------
 # make_data - will take a list and
 # make the data to send even encode(FORMAT)
 def make_data(all_data):
     full_data = ""
-    token = "$"
+
     for data in all_data:
-        full_data += data + token
+        full_data += data + TOKEN_STR
     print(full_data)
     return full_data.encode(FORMAT)
 
 
 def id_generator(size=128, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-
 
 
 def make_dir(user_id):
@@ -53,7 +56,6 @@ def make_dir(user_id):
 
 # -------------------
 # clear_folder will delete all the content of the folder
-
 def clear_folder(dir):
     if os.path.exists(dir):
         for the_file in os.listdir(dir):
@@ -74,82 +76,76 @@ def main():
     identifier = ""
     user_comp_id_connected = ""
 
-    token = "$"
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('', PORT))
     server.listen(5)
     print(f"[LISTENING] Server is listening on {PORT}.")
     size = 1024
     while True:
-        conn, addr = server.accept()
+        client, addr = server.accept()
         print(f"[NEW CONNECTION] {addr} connected.")
 
         while True:
-            data = conn.recv(size)
-            data = data.split(b"$")
+            data = client.recv(size)
+            data = data.split(TOKEN_IN_BYTES)
             cmd = data[0]
             # to set the size of the commend
             if cmd == b"SET_SIZE":
                 size = int(data[1].decode(FORMAT))
-                print(size)
+                print("SIZE of next pact =" + str(size))
                 break
             # new user is connecting
             if cmd == b"NEW_USER":
-                generate = id_generator()
-                identifier = generate
+                identifier = id_generator()
                 user_comp_id_connected = data[1].decode(FORMAT)
-                make_dir(generate)
-                send_data = "NEW_USER_ID" + token + generate
-                conn.send(send_data.encode(FORMAT))
+                make_dir(identifier)
+                send_data = make_data(["NEW_USER_ID", identifier])
+                client.send(send_data)
+                print("new user identifier = " + identifier)
                 break
 
-            # user with a user name concect
+            # user with a user name connect will receive al the data contain in the file
             if cmd == b"CONNECT":
                 identifier = data[2].decode(FORMAT)
                 user_comp_id_connected = data[1].decode(FORMAT)
                 path = os.path.join(os.getcwd(), identifier)
                 for root, dirs, files in os.walk(path, topdown=True):
 
-
                     for name in files:
-                        full_path = os.path.join(root,name)
-                        deliver_path = return_path(full_path,identifier)
+                        full_path = os.path.join(root, name)
+                        deliver_path = return_path(full_path, identifier)
                         size_of_next_data = sys.getsizeof(make_data(['UPLOAD_FILE', deliver_path]))
-                        conn.send(make_data(['SET_SIZE', str(size_of_next_data)]))
+                        client.send(make_data(['SET_SIZE', str(size_of_next_data)]))
 
                         print(name)
                         file_size = os.path.getsize(full_path)
-                        conn.recv(REC_ACK)
-                        conn.send(make_data(['UPLOAD_FILE', deliver_path[1:], str(file_size)]))
+                        client.recv(REC_ACK)
+                        client.send(make_data(['UPLOAD_FILE', deliver_path[1:], str(file_size)]))
                         print(make_data(['UPLOAD_FILE', deliver_path[1:], str(file_size)]))
-                        conn.recv(REC_ACK)
+                        client.recv(REC_ACK)
 
                         file_to_send = open(full_path, "rb")
 
-
                         data = file_to_send.read()
-                        conn.send(data)
+                        client.send(data)
 
                         file_to_send.close()
-                        conn.recv(REC_ACK)
-                        print("done sending")
+                        client.recv(REC_ACK)
+                        print("done sending file name" + name)
 
                     for name in dirs:
                         full_path = os.path.join(root, name)
                         deliver_path = return_path(full_path, identifier)
                         size_of_next_data = sys.getsizeof(make_data(['CRF', deliver_path]))
-                        conn.send(make_data(['SET_SIZE', str(size_of_next_data)]))
-                        conn.recv(REC_ACK)
-                        # time.sleep(0.1)
-                        print(make_data(['CRF', deliver_path[1:]]))
-                        conn.send(make_data(['CRF', deliver_path[1:]]))
-                        conn.recv(REC_ACK)
-                        print(name)
+                        client.send(make_data(['SET_SIZE', str(size_of_next_data)]))
+                        client.recv(REC_ACK)
 
-                make_dir(identifier)
-                print(identifier)
-                conn.send(b"DONE")
-                print("don sending")
+                        client.send(make_data(['CRF', deliver_path[1:]]))
+                        client.recv(REC_ACK)
+                        print("send client to create dir-" + name)
+                # sending the client
+                client.send(b"DONE")
+                print("done sending to client" + identifier + "update")
                 break
 
             # makes a dir
@@ -160,41 +156,42 @@ def main():
             # uplod file
             elif cmd == b"UPLOAD":
 
-                path_to_put = data[1].decode(FORMAT)
-                print(path_to_put)
-                filepath = SERVER_DATA_PATH + "/" + identifier + path_to_put  #######there is "/" befor path_to_put
-                print(filepath)
+                path_to_put = data[1].decode(FORMAT)[1:]
+                filepath = os.path.join(SERVER_DATA_PATH, identifier,
+                                        path_to_put)  # we need to check after change
+
                 # right to the file the content that it receive
-                filetodown = open(filepath, "wb")
-                l = conn.recv(1024)
-                while (l):
-                    print("Receiving...")
+                file_to_down = open(filepath, "wb")
+                len_of_data_rec = client.recv(1024)
+                while len_of_data_rec:
+                    file_to_down.write(len_of_data_rec)
+                    len_of_data_rec = client.recv(1024)
+                file_to_down.close()
 
-                    filetodown.write(l)
-                    l = conn.recv(1024)
-                filetodown.close()
-
-                print("Done Receiving.")
-                filetodown.close()
+                print("Done Receiving the file. " + filepath)
+                file_to_down.close()
                 break
             # delete the file
             elif cmd == b"DELETE":
-                event_path_del = data[1].decode(FORMAT)
-                path_del = SERVER_DATA_PATH + "/" + identifier + event_path_del
-                # if its a file
+                event_path_del = data[1].decode(FORMAT)[1:]
+                path_del = os.path.join(SERVER_DATA_PATH, identifier, event_path_del)  # check after change
+                # if its a file it will delete the file
                 if os.path.isfile(path_del):
                     os.unlink(path_del)
 
-                else:
+                else:  # if it a dir it will delete the dir
                     clear_folder(path_del)
-                print(path_del + "-DELETE")
+                print("DELETE-" + path_del)
                 break
+            # move file from source_path to destination_path
             elif cmd == b"MOVE":
-                os.replace(os.path.join(SERVER_DATA_PATH, identifier, data[1].decode(FORMAT)),
-                           os.path.join(SERVER_DATA_PATH, identifier, data[2].decode(FORMAT)))
+                source_path = os.path.join(SERVER_DATA_PATH, identifier, data[1].decode(FORMAT))
+                destination_path = os.path.join(SERVER_DATA_PATH, identifier, data[2].decode(FORMAT))
+                os.replace(source_path, destination_path)
+                print("file moved from- " + source_path + "to" + destination_path)
 
         print(f"[DISCONNECTED] {addr} disconnected")
-        conn.close()
+        client.close()
 
 
 if __name__ == "__main__":
